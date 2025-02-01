8835070e24a56c3883637bfd488df71609a349d3
Ayeasha03 <oyinaishat3@gmail.com>
2025-02-01 23:15:19 +0100

    Normalize line endings

--- src/components/sections/contact/contact.jsx
+++ src/components/sections/contact/contact.jsx
@@ -22,7 +22,7 @@ function Contact() {
     e.preventDefault();
     const id = toast.loading("Sending message...");
     try {
-      const response = await axios.post("http://localhost:9000/api/messages/", formData);  // Adjust the URL as needed
+      const response = await axios.post("https://backend-o2rt.onrender.com/api/messages/", formData);  // Adjust the URL as needed
       toast.update(id, {
         render: "Message sent successfully!",
         type: "success",
